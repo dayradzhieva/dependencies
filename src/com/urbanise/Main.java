c34e32b9f641419026a2e5d026420f2be92e0b58
dayradzhieva <tomi.dayradzhieva@gmail.com>
2022-11-02 12:20:02 +0200

    commit

--- src/com/urbanise/Main.java
+++ src/com/urbanise/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Map<String, Set<String>> dependencies = new HashMap<>();
+        Map<String, Set<String>> analysedDependencies = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
 
         String line;
@@ -20,9 +21,13 @@ public class Main {
         }
 
         for (String rootDependency: dependencies.keySet()) {
-            System.out.print(rootDependency + " ");
-            print(dependencies, rootDependency);
-            System.out.println("");
+            Set<String> allDependencies = dependencyAnalysis(dependencies, rootDependency);
+            analysedDependencies.putIfAbsent(rootDependency, allDependencies);
+        }
+
+
+        for (String rootDependency: analysedDependencies.keySet()) {
+            print(analysedDependencies, rootDependency);
         }
     }
 
@@ -38,24 +43,39 @@ public class Main {
         }
     }
 
+    private static Set<String> dependencyAnalysis (Map<String, Set<String>> dependencies, String rootDependency){
+        Set<String> analysedDependencies = dependencies.get(rootDependency);
+        Set<String> subDependencies;
+
+        for (String root: dependencies.keySet()) {
+            if(root.equals(rootDependency)){
+                continue;
+            }
+            subDependencies = dependencies.get(root);
+            if(analysedDependencies.contains(root)) {
+                analysedDependencies.addAll(subDependencies);
+            }
+        }
+        return analysedDependencies;
+    }
+
     private static void print(Map<String, Set<String>> dependencies, String rootDependency) {
         if (!dependencies.containsKey(rootDependency)) {
             return;
         }
-
         Set<String> subDependencies = dependencies.get(rootDependency);
 
+        System.out.print(rootDependency + " ");
 
         for (String subDependency: subDependencies) {
             System.out.print(subDependency + " ");
-            print(dependencies, subDependency);
         }
-
-//        A B C
-//        B C E
-//        C G
-//        D A F
-//        E F
-//        F H
+        System.out.println();
     }
+    //        A B C
+    //        B C E
+    //        C G
+    //        D A F
+    //        E F
+    //        F H
 }
